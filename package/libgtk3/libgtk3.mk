4dab29e2e03988c2b8d2c5ea019aff04e84f4779
HeavyPunk <kirillkuchma8@gmail.com>
2025-12-06 16:14:42 +0500

    add libgtk3

--- package/libgtk3/libgtk3.mk
+++ package/libgtk3/libgtk3.mk
@@ -7,7 +7,7 @@
 LIBGTK3_VERSION_MAJOR = 3.24
 LIBGTK3_VERSION = $(LIBGTK3_VERSION_MAJOR).43
 LIBGTK3_SOURCE = gtk+-$(LIBGTK3_VERSION).tar.xz
-LIBGTK3_SITE = https://download.gnome.org/sources/gtk+/$(LIBGTK3_VERSION_MAJOR)
+LIBGTK3_SITE = https://github.com/HeavyPunk/batocera.packages/raw/refs/heads/main
 LIBGTK3_LICENSE = LGPL-2.0+
 LIBGTK3_LICENSE_FILES = COPYING
 LIBGTK3_CPE_ID_VENDOR = gnome
