5058d280d2e9cd40694d75ba6f012845f8a39c38
HeavyPunk <kirillkuchma8@gmail.com>
2025-12-06 18:19:19 +0500

    add b43-fwcutter

--- package/b43-fwcutter/b43-fwcutter.mk
+++ package/b43-fwcutter/b43-fwcutter.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 B43_FWCUTTER_VERSION = 019
-B43_FWCUTTER_SITE = http://bues.ch/b43/fwcutter
+B43_FWCUTTER_SITE = https://github.com/HeavyPunk/batocera.packages/raw/refs/heads/main
 B43_FWCUTTER_SOURCE = b43-fwcutter-$(B43_FWCUTTER_VERSION).tar.bz2
 B43_FWCUTTER_LICENSE = BSD-2-Clause
 B43_FWCUTTER_LICENSE_FILES = COPYING
